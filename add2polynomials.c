240e9b3fe3209d3416cd82ba847a457fcda48736
Rahul <rahulgunturu123@gmail.com>
2025-07-28 18:48:48 +0530

    Working code and cleaned version (needs a little adjustment)

--- add2polynomials.c
+++ add2polynomials.c
@@ -1,107 +1,95 @@
 #include<stdio.h>
 #include<stdlib.h>
-#include<math.h>
 
 struct node
 {
-    int coefficient;
-    int exponent;
-    struct node* next;
-}*head1 = NULL , *head2 = NULL , *ptr1 = NULL , *ptr2 = NULL , *temp = NULL , *head3 = NULL , *ptr3 = NULL;
+    int coeff;
+    int expo;
+    struct node *next;
+}*head1 = NULL , *head2 = NULL;
 
-struct node* newLinkedList(struct node* head , struct node* ptr , int NOT)
+struct node *nodeCreator(struct node *head , int coeff , int expo)
 {
-    while (NOT>0)
+    struct node* temp = (struct node*)malloc(sizeof(struct node));
+    temp -> coeff = coeff;
+    temp -> expo = expo;
+    temp -> next = NULL;
+    if (head == NULL) head = temp;
+    else
     {
-        temp = (struct node*)malloc(sizeof(struct node));
-        printf("Enter coefficient: ");
-        scanf("%d" , &temp->coefficient);
-        temp->exponent = NOT-1;
-        temp->next = NULL;
-        if(head == NULL) head = temp;
-        else ptr->next = temp;
-        ptr = temp;
-        NOT--;
+        struct node* current = head;
+        while (current->next!=NULL) current = current->next;
+        current->next = temp;
     }
     return head;
+}
+
+void displayNode(struct node* head)
+{
+    while (head!=NULL)
+    {
+        if(head->expo != 0) printf("%dx^%d + " , head->coeff , head->expo);
+        else printf("%d" , head->coeff);
+        head = head->next;
+    }
+    printf("\n");
+}
+
+struct node* addPolynomials(struct node* head1,struct node *head2)
+{
+    struct node* result = NULL;
+    while (head1 != NULL && head2 != NULL)
+    {
+        if(head1->expo == head2->expo)
+        {
+            result = nodeCreator(result , head1->coeff + head2->coeff , head1->expo);
+            head1 = head1->next;
+            head2 = head2->next;
+        }
+        else if(head1->expo > head2->expo)
+        {
+            result = nodeCreator(result , head1->coeff , head1->expo);
+            head1 = head1->next;
+        }
+        else
+        {
+            result = nodeCreator(result , head2->coeff , head2->expo);
+            head2 = head2->next;
+        }
+    }
+    return result;
 }
 
 int main()
 {
     int numberOfTerms;
-    printf("--------------- FIRST POLYNOMIAL ---------------\n");
-    printf("Enter number of terms you want to enter: ");
-    scanf("%d",&numberOfTerms);
-    head1 = newLinkedList(head1 , ptr1 , numberOfTerms);
-
-    printf("\n--------------- SECOND POLYNOMIAL ---------------\n");
-    printf("Enter number of terms you want to enter: ");
-    scanf("%d",&numberOfTerms);
-    head2 = newLinkedList(head2 , ptr2 , numberOfTerms);
-    ptr1 = head1;
-    ptr2 = head2;
-    while (ptr1 != NULL && ptr2 != NULL)
+    printf("---------FIRST POLYNOMIAL--------\n");
+    printf("Enter number of terms: ");
+    scanf("%d" , &numberOfTerms);
+    while (numberOfTerms > 0)
     {
-        int sum;
-        if(ptr1->exponent == ptr2->exponent)
-        {
-            sum = ptr1->coefficient + ptr2->coefficient;
-            temp = (struct node*)malloc(sizeof(struct node));
-            temp->coefficient = sum;
-            temp->exponent = ptr1->exponent;
-            temp->next = NULL;
-            ptr1 = ptr1->next;
-            ptr2 = ptr2->next;
-        }
-        else if(ptr1->exponent > ptr2->exponent)
-        {
-            sum = ptr1->coefficient;
-            temp = (struct node*)malloc(sizeof(struct node));
-            temp->coefficient = sum;
-            temp->exponent = ptr1->exponent;
-            temp->next = NULL;
-            ptr1 = ptr1->next;
-        }
-        else
-        {
-            sum = ptr2->coefficient;
-            temp = (struct node*)malloc(sizeof(struct node));
-            temp->coefficient = sum;
-            temp->exponent = ptr2->exponent;
-            temp->next = NULL;
-            ptr2 = ptr2->next;
-        }
-        if(head3 == NULL) head3 = temp;
-        else ptr3->next = temp;
-        ptr3 = temp;
+        int mainCoeff , mainExpo;
+        printf("Enter coefficient and exponent respectively: ");
+        scanf("%d %d",&mainCoeff,&mainExpo);
+        head1 = nodeCreator(head1 , mainCoeff , mainExpo);
+        numberOfTerms--;
     }
 
-    //Printing
-    printf("Polynomial 1 : ");
-    ptr1 = head1;
-    while (ptr1 != NULL)
+    printf("---------SECOND POLYNOMIAL--------\n");
+    printf("Enter number of terms: ");
+    scanf("%d",&numberOfTerms);
+    while (numberOfTerms>0)
     {
-        if(ptr1->exponent != 0) printf("%dx^%d + " , ptr1->coefficient , ptr1->exponent);
-        else printf("%d" , ptr1->coefficient);
-        ptr1 = ptr1->next;
+        int mainCoeff , mainExpo;
+        printf("Enter coefficient and exponent respectively: ");
+        scanf("%d %d",&mainCoeff,&mainExpo);
+        head2 = nodeCreator(head2 , mainCoeff , mainExpo);
+        numberOfTerms--;
     }
-    printf("\n");
-    printf("Polynomial 2 : ");
-    ptr2 = head2;
-    while (ptr2 != NULL)
-    {
-        if(ptr2->exponent != 0) printf("%dx^%d + " , ptr2->coefficient , ptr2->exponent);
-        else printf("%d" , ptr2->coefficient);
-        ptr2 = ptr2->next;
-    }
-    printf("\n");
-    printf("Sum: ");
-    ptr3 = head3;
-    while (ptr3 != NULL)
-    {
-        if(ptr3->exponent != 0) printf("%dx^%d + " , ptr3->coefficient , ptr3->exponent);
-        else printf("%d",ptr3->coefficient);
-        ptr3 = ptr3->next;
-    }
-    
+    printf("Polynimial 1: ");
+    displayNode(head1);
+    printf("Polynomial 2: ");
+    displayNode(head2);
+    printf("Sum of the polynomials is: ");
+    displayNode(addPolynomials(head1,head2));
 }
